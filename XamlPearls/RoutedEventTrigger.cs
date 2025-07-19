cf18936e71fef53cbbc481452f65d5d99afdbf61
Bruce Lee <li.liuwei@outlook.com>
2025-07-19 22:24:57 +0800

    format RoutedEventTrigger

--- XamlPearls/RoutedEventTrigger.cs
+++ XamlPearls/RoutedEventTrigger.cs
@@ -6,32 +6,53 @@ namespace XamlPearls
 {
     public class RoutedEventTrigger : EventTriggerBase<DependencyObject>
     {
-        public RoutedEvent RoutedEvent { get; set; }
+        private RoutedEvent routedEvent;
 
-        protected override void OnAttached()
+        public RoutedEventTrigger()
         {
-            Behavior behavior = base.AssociatedObject as Behavior;
-            FrameworkElement associatedElement = base.AssociatedObject as FrameworkElement;
-            if (behavior != null)
-            {
-                associatedElement = ((IAttachedObject)behavior).AssociatedObject as FrameworkElement;
-            }
-            if (associatedElement == null)
-            {
-                throw new ArgumentException("Routed Event trigger can only be associated to framework elements");
-            }
-
-            associatedElement.AddHandler(RoutedEvent, new RoutedEventHandler(this.OnRoutedEvent));
         }
 
-        private void OnRoutedEvent(object sender, RoutedEventArgs args)
+        public RoutedEvent RoutedEvent
         {
-            base.OnEvent(args);
+            get
+            {
+                return this.routedEvent;
+            }
+
+            set
+            {
+                this.routedEvent = value;
+            }
         }
 
         protected override string GetEventName()
         {
             return RoutedEvent.Name;
+        }
+
+        protected override void OnAttached()
+        {
+            Behavior behavior = this.AssociatedObject as Behavior;
+            FrameworkElement associatedElement = this.AssociatedObject as FrameworkElement;
+            if (behavior != null)
+            {
+                associatedElement = ((IAttachedObject)behavior).AssociatedObject as FrameworkElement;
+            }
+
+            if (associatedElement == null)
+            {
+                throw new ArgumentException("Routed Event trigger can only be associated to framework elements");
+            }
+
+            if (RoutedEvent != null)
+            {
+                associatedElement.AddHandler(RoutedEvent, new RoutedEventHandler(this.OnRoutedEvent));
+            }
+        }
+
+        private void OnRoutedEvent(object sender, RoutedEventArgs args)
+        {
+            this.OnEvent(args);
         }
     }
 }
