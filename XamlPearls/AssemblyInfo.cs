14befbf5e10a7affe638c9b83fc14ef51a0a9731
value lee <value.lee@outlook.com>
2026-07-12 06:52:42 +0800

    backup

--- XamlPearls/AssemblyInfo.cs
+++ XamlPearls/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Markup;
 
+[assembly: XmlnsDefinition("https://github.com/atomtick/XamlPearls", "XamlPearls.Miscellaneous")]
 [assembly: XmlnsDefinition("https://github.com/atomtick/XamlPearls", "XamlPearls.OneSecond")]
 [assembly: XmlnsDefinition("https://github.com/atomtick/XamlPearls", "XamlPearls.XamlFont")]
 [assembly: XmlnsDefinition("https://github.com/atomtick/XamlPearls", "XamlPearls")]
